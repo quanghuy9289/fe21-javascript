3187aaa1f6c4bdde7c18071881aac7e66b952024
lephuquan <lephuquan2k1@gmail.com>
2021-12-29 14:07:12 +0700

    push ch05

--- Ch05/faqs/faqs.js
+++ Ch05/faqs/faqs.js
@@ -3,33 +3,38 @@ var $ = function (id) { return document.getElementById(id); };
 
 // the event handler for the click event of each h2 element
 var toggle = function () {
-    var h2 = this;                    // clicked h2 tag     
-    var div = h2.nextElementSibling;  // h2 tag's sibling div tag
+    var a = this;
+    var div = a.parentNode.nextElementSibling;  // trỏ đến node cha-parent(h2) để dùng nexE.. trỏ đến node div
 
     // toggle plus and minus image in h2 elements by adding or removing a class
-    if (h2.hasAttribute("class")) { // nếu h2 đã có class
-        h2.removeAttribute("class");	// xóa class đó đi
+    if (a.hasAttribute("class")) {
+        // a.removeAttribute("class");
+        a.className = "";
     } else {
-        h2.setAttribute("class", "minus"); // tạo mới 1 class tên là minus
+        // a.setAttribute("class", "minus"); 
+        a.className = "minus";
+
     }
 
     // toggle div visibility by adding or removing a class
     if (div.hasAttribute("class")) {
-        div.removeAttribute("class");
+        // div.removeAttribute("class");
+        div.className = "";
     } else {
-        div.setAttribute("class", "open");
+        // div.setAttribute("class", "open");
+        div.className = "open";
     }
 };
 
 window.onload = function () {
     // get the h2 tags
     var faqs = $("faqs");
-    var h2Elements = faqs.getElementsByTagName("h2");
+    var aElements = faqs.getElementsByTagName("a");
 
     // attach event handler for each h2 tag	    
-    for (var i = 0; i < h2Elements.length; i++) {
-        h2Elements[i].onclick = toggle;
+    for (var i = 0; i < aElements.length; i++) {
+        aElements[i].onclick = toggle;
     }
     // set focus on first h2 tag's <a> tag
-    h2Elements[0].firstChild.focus();
+    aElements[0].firstChild.focus();
 };
--- Ch05/register_table/register.js
+++ Ch05/register_table/register.js
@@ -1,7 +1,7 @@
 "use strict";
-var $ = function(id) { return document.getElementById(id); };
+var $ = function (id) { return document.getElementById(id); };
 
-var processEntries = function() {   
+var processEntries = function () {
     var header = "";
     var html = "";
     var required = "<span>Required field</span>";
@@ -10,34 +10,36 @@ var processEntries = function() {
     var phone = $("phone").value;
     var country = $("country").value;
     var contact = "Text";
+    var comments = $("comments").value;
     if ($("email").checked) { contact = "Email"; }
     if ($("none").checked) { contact = "None"; }
     var terms = $("terms").checked;
 
     if (email == "") {
-        email = required;          
+        email = required;
         header = msg;
     }
-	if (phone == "") {
-        phone = required;         
+    if (phone == "") {
+        phone = required;
         header = msg;
     }
-	if (country == "") {
-        country = required;         
+    if (country == "") {
+        country = required;
         header = msg;
     }
     if (terms == false) {
         terms = required;
         header = msg;
     }
-    
+
     $("registration_header").firstChild.nodeValue = header;
-    if (header == msg) {     
+    if (header == msg) {
         html = html + "<tr><td>Email:</td><td>" + email + "</td></tr>";
         html = html + "<tr><td>Phone:</td><td>" + phone + "</td></tr>";
         html = html + "<tr><td>Country:</td><td>" + country + "</td></tr>";
         html = html + "<tr><td>Contact:</td><td>" + contact + "</td></tr>";
         html = html + "<tr><td>Terms:</td><td>" + terms + "</td></tr>";
+        html = html + "<tr><td>Length =</td><td>" + comments.length + "</td></tr>";
         $("registration_info").innerHTML = html;
     } else {
         $("registration_info").innerHTML = "";
@@ -45,15 +47,15 @@ var processEntries = function() {
     }
 };
 
-var resetForm = function() {
+var resetForm = function () {
     $("registration_form").reset();
     $("registration_header").firstChild.nodeValue = "";
     $("registration_info").innerHTML = "";
     $("email_address").focus();
 };
 
-window.onload = function() {
+window.onload = function () {
     $("register").onclick = processEntries;
-    $("reset_form").onclick = resetForm;    
+    $("reset_form").onclick = resetForm;
     $("email_address").focus();
 };
